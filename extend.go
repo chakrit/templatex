3846acd1dfabb5fab2385013e19242a66e2ecde8
Chakrit Wichian <service@chakrit.net>
2015-02-07 13:47:45 +0700

    Simpler implementation with less recursion.

--- extend.go
+++ extend.go
@@ -1,20 +1,23 @@
 package main
 
 import "os"
-import "text/template"
+import "html/template"
 import "text/template/parse"
+import "sort"
+import "fmt"
 
-const TemplateName = "main.template"
+const BaseTemplateName = "base.template"
+const ChildTemplateName = "main.template"
 
 func main() {
 	must := template.Must
 	funcs := template.FuncMap{
-		"extend": template_Extend,
+		"extends": template_Extend,
 	}
 
-	t := must(template.New("").Funcs(funcs).ParseFiles(TemplateName))
+	t := must(template.New("").Funcs(funcs).ParseFiles(ChildTemplateName))
 	t = must(preprocess(t))
-	if e := t.Execute(os.Stdout, nil); e != nil {
+	if e := t.ExecuteTemplate(os.Stdout, BaseTemplateName, nil); e != nil {
 		panic(e)
 	}
 }
@@ -24,84 +27,98 @@ func template_Extend(name string) string {
 	return ""
 }
 
+func log(msg string, args ...interface{}) {
+	fmt.Errorf(msg, args...)
+}
+
 func preprocess(t *template.Template) (*template.Template, error) {
-	if t.Tree != nil {
-		return preprocessTree(t, t.Tree)
+	result, updated := t, false
+	lookup := map[string]*template.Template{}
+	extends := listExtends(t)
+
+	lookup[result.Name()] = result
+	for _, template := range result.Templates() {
+		lookup[template.Name()] = template
 	}
 
-	result, children := t, t.Templates()
-	for _, child := range children {
-		if updated, e := preprocess(child); e != nil {
-			return nil, e
-		} else if updated != nil {
-			result = updated
+	for _, extension := range extends {
+		if _, ok := lookup[extension]; !ok {
+			log("extends:", extension)
+			if update, e := t.ParseFiles(extension); e != nil {
+				return result, e
+			} else {
+				result = update
+				updated = true
+			}
 		}
+	}
+
+	if updated {
+		return preprocess(result) // until we have no updates
 	}
 
 	return result, nil
 }
 
-func preprocessTree(t *template.Template, tree *parse.Tree) (*template.Template, error) {
-	return preprocessNode(t, tree.Root)
+func listExtends(t *template.Template) []string {
+	log("processing:", t.Name())
+
+	templates := []*template.Template{t}
+	for _, template := range t.Templates() {
+		templates = append(templates, template)
+	}
+
+	nodes := []parse.Node{}
+	for _, template := range templates {
+		if template.Tree != nil {
+			log("root:", template.Name())
+			nodes = append(nodes, template.Tree.Root)
+		}
+	}
+
+	result := []string{}
+	for _, node := range nodes {
+		result = append(result, listNodeExtends(node)...)
+	}
+
+	sort.StringSlice(result).Sort()
+	return result
 }
 
-func preprocessNode(t *template.Template, node parse.Node) (result *template.Template, e error) {
-	defer func() {
-		if recov := recover(); recov != nil {
-			if err, ok := recov.(error); ok {
-				result, e = nil, err
-			}
-		}
-	}()
-
-	result = t
-	descend := func(child parse.Node) {
-		if updated, e := preprocessNode(result, child); e != nil {
-			panic(e)
-		} else if updated != nil {
-			result = updated
-		}
+func listNodeExtends(node parse.Node) []string {
+	result := []string{}
+	include := func(node parse.Node) {
+		result = append(result, listNodeExtends(node)...)
 	}
 
 	switch n := node.(type) {
 	case *parse.ListNode:
 		for _, child := range n.Nodes {
-			descend(child)
+			include(child)
 		}
 	case *parse.ActionNode:
-		descend(n.Pipe)
+		include(n.Pipe)
 	case *parse.PipeNode:
 		for _, child := range n.Cmds {
-			descend(child)
+			include(child)
 		}
 
 	case *parse.CommandNode:
 		if len(n.Args) == 2 {
-			if ident, ok := n.Args[0].(*parse.IdentifierNode); ok && ident.Ident == "extend" {
+			if ident, ok := n.Args[0].(*parse.IdentifierNode); ok && ident.Ident == "extends" {
 				if extendee, ok := n.Args[1].(*parse.StringNode); ok && len(extendee.Text) > 0 {
-					result, e = preprocessExtend(result, extendee.Text)
-					break
+					result = append(result, extendee.Text)
 				}
 			}
 		}
 
 		for _, child := range n.Args {
-			descend(child)
+			include(child)
 		}
 
 	default:
-		return nil, nil // ignore all other node types.
+		return []string{} // ignore all other node types.
 	}
 
-	return result, nil
-}
-
-func preprocessExtend(tmpl *template.Template, filename string) (*template.Template, error) {
-	if extendee, e := template.ParseFiles(filename); e != nil {
-		return nil, e
-	} else if extendee != nil {
-		return tmpl.AddParseTree(filename, extendee.Tree)
-	}
-
-	return tmpl, nil
+	return result
 }
